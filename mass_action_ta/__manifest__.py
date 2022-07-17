b450a93dc3d8e8f7f382901c19046b07804d5ca1
ABXTTI <49796896+ABXTTI@users.noreply.github.com>
2022-07-17 19:22:25 +0500

    Add files via upload

---
+++ mass_action_ta/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Mass Action TA',
+    'version' : '1.2',
+    'summary': 'Mass Action',
+    'sequence': 11,
+    'description': """
+Invoicing & Payments
+====================
+The specific and easy-to-use Invoicing system in Odoo allows you to keep track of your accounting, even when you are not an accountant. It provides an easy way to follow up on your vendors and customers.
+
+You could use this simplified accounting in case you work with an (external) account to keep your books, and you still want to keep track of payments. This module also offers you an easy method of registering payments, without having to encode complete abstracts of account.
+    """,
+    'category': 'Accounting/Accounting',
+    'website': 'https://www.odoo.com/app/invoicing',
+    'images' : [],
+    'depends' : ['base', 'account','base_accounting_kit'],
+    'data': [
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
+        'views/mass_action_account_move.xml',
+        # 'views/cpp.xml',
+        # 'views/account_move_line_inherit.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'assets': {
+        'web._assets_primary_variables': [],
+        'web.assets_backend': [],
+        'web.assets_frontend': [],
+        'web.assets_tests': [],
+        'web.qunit_suite_tests': [],
+        'web.assets_qweb': [],
+    },
+    'license': 'LGPL-3',
+}
---
+++ mass_action_ta/models/__init__.py
@@ -0,0 +1,1 @@
+from . import mass_action_account_move
