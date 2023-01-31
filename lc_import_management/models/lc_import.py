8f04c44d4a4387d9bb37b280440ec4018a67e821
ABXTTI <49796896+ABXTTI@users.noreply.github.com>
2023-02-01 04:57:45 +0500

    Add files via upload

--- lc_import_management/models/lc_import.py
+++ lc_import_management/models/lc_import.py
@@ -6,6 +6,9 @@ class LcImport(models.Model):
     name = fields.Char(string="LC #", required=1)
     description = fields.Char(string="Description", required=1)
     company_id = fields.Many2one("res.company", string="Company", default=lambda self: self.env.company)
+    purchase_order_ids = fields.One2many('purchase.order', 'lc_number', string="Purchase Orders")
+    account_move_ids = fields.One2many('account.move', 'lc_number', string="Bills")
+    account_payment_ids = fields.One2many('account.payment', 'lc_number', string="Payments")
 
     _sql_constraints = [
         ('unique_name_per_company', 'unique (name,company_id)', 'Name Must Be Unique !')
